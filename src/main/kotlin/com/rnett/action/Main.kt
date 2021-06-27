60448723406d7f66086a2c0d1c63216e5c31d659
Ryan Nett <JNett96@gmail.com>
2021-06-27 16:51:04 -0700

    Use withDefault

--- src/main/kotlin/com/rnett/action/Main.kt
+++ src/main/kotlin/com/rnett/action/Main.kt
@@ -4,20 +4,24 @@ import com.rnett.action.core.fail
 import com.rnett.action.core.inputs
 import com.rnett.action.core.logger
 import com.rnett.action.core.runAction
-import com.rnett.action.core.runOrFail
-import com.rnett.action.delegates.ifNull
 import com.rnett.action.delegates.isTrue
 import com.rnett.action.exec.exec
 
-fun parseLocation(location: String, version: String?, isSnapshot: Boolean?, latestSnapshot: String, latestRelease: String): String {
-    if("\$version" in location && version == null){
+fun parseLocation(
+    location: String,
+    version: String?,
+    isSnapshot: Boolean?,
+    latestSnapshot: String,
+    latestRelease: String
+): String {
+    if ("\$version" in location && version == null) {
         fail("\$version used in publish-to, but version is not set.")
     }
-    if("\$latest" in location && version == null){
+    if ("\$latest" in location && version == null) {
         fail("\$latest used in publish-to, but version is not set.")
     }
     return location.replace("\$version", version ?: "")
-        .replace("\$latest", if(isSnapshot == true) latestSnapshot else latestRelease)
+        .replace("\$latest", if (isSnapshot == true) latestSnapshot else latestRelease)
         .replace("\\,", ",")
         .replace("\\\\", "\\")
 }
@@ -25,7 +29,7 @@ fun parseLocation(location: String, version: String?, isSnapshot: Boolean?, late
 suspend fun updateDocs(folder: Path, from: Path, delete: Boolean) {
     folder.mkdir()
     logger.info("Copying docs to $folder")
-    if(delete)
+    if (delete)
         folder.children.forEach { it.delete(true) }
     from.copyChildrenInto(folder)
 }
@@ -35,10 +39,10 @@ suspend fun main() = runAction {
     val from by inputs.optional
     val fromFile by inputs.optional
 
-    if(from.isNullOrBlank() && fromFile.isNullOrBlank()){
+    if (from.isNullOrBlank() && fromFile.isNullOrBlank()) {
         fail("Should only specify one of 'from' and 'from-file'.")
     }
-    if(from.isNullOrBlank() && fromFile.isNullOrBlank()){
+    if (from.isNullOrBlank() && fromFile.isNullOrBlank()) {
         fail("Must specify exactly one of: 'from', 'from-file'.")
     }
 
@@ -50,14 +54,14 @@ suspend fun main() = runAction {
     val version by inputs.optional
 
     val (latestSnapshot, latestRelease) = inputs.getOrElse("latests") { "snapshot|release" }.let {
-        if('|' !in it || it.count { it == '|' } > 1)
+        if ('|' !in it || it.count { it == '|' } > 1)
             fail("latests must be two strings seperated by a '|'")
         it.substringBefore('|') to it.substringAfter('|')
     }
 
     val restore by inputs.isTrue()
 
-    val message by inputs.optional.ifNull { if (version != null) "Docs for \$version" else "Docs update" }
+    val message by inputs.optionalWithDefault { if (version != null) "Docs for \$version" else "Docs update" }
 
     if ("\$version" in message && version == null)
         fail("'\$version' used in message, but version not set.")
@@ -72,7 +76,7 @@ suspend fun main() = runAction {
     val publishTo = inputs.getOrElse("publish-to") { if (version != null) "\$version,\$latest" else "." }
         .split(',').filter { it.isNotBlank() }
         .filterNot {
-            when(isSnapshot){
+            when (isSnapshot) {
                 true -> it.startsWith('!')
                 false -> it.startsWith('?')
                 else -> false
@@ -84,7 +88,7 @@ suspend fun main() = runAction {
         mkdir()
         children.forEach { it.delete(true) }
     }
-    if(!from.isNullOrBlank())
+    if (!from.isNullOrBlank())
         Path(from!!).moveChildrenInto(fromPath)
     else
         Path(fromFile!!).move(fromPath)
@@ -104,7 +108,7 @@ suspend fun main() = runAction {
         }
     }
 
-    if(restoreDir != null){
+    if (restoreDir != null) {
         (Path.cwd / ".git").copy(restoreDir)
     }
 
@@ -126,14 +130,14 @@ suspend fun main() = runAction {
 
     val changed = exec.execCommand("git diff-index --exit-code --quiet --cached HEAD", ignoreReturnCode = true) != 0
 
-    if(changed) {
+    if (changed) {
         exec.execCommand(
             "git -c user.name=\'$authorName\' -c user.email=\'$authorEmail\' " +
                     "commit -q -m \"${message.replace("\$version", version ?: "")}\""
         )
 
         exec.execCommand("git push origin $branch")
-    } else{
+    } else {
         println("No changes")
     }
 
